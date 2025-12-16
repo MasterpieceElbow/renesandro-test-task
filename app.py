4d0b21102cdb1e206d71bf3d465e74971abf0c6b
Oleksii Proshchenko <80070761+MasterpieceElbow@users.noreply.github.com>
2025-12-16 12:10:37 +0200

    fix app.py

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-try:
-    1 / 0
+import os
+from fastapi import FastAPI
 
-except Exception as e:
-    raise e.__class__(f"An error occurred during division: {str(e)}")
+from routers.routers import router
+
+
+def create_app() -> FastAPI:
+    app = FastAPI()
+    app.include_router(router)
+    return app
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=port)
